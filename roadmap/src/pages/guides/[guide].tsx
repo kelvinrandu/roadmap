f8536efe24b83e21f92f6e6152372c0c1a576725
kelvinrandu <randukelvin@gmail.com>
2023-01-18 22:26:55 +0300

    guides and videos pages

--- roadmap/src/pages/guides/[guide].tsx
+++ roadmap/src/pages/guides/[guide].tsx
@@ -1,9 +1,85 @@
-import React, { Component } from 'react'
+import { Box, Container } from "@chakra-ui/react";
+import { GlobalHeader } from "../../components/global-header";
+import { Footer } from "../../components/footer";
+import { ContentPageHeader } from "../../components/content-page-header";
+import MdRenderer from "../../components/md-renderer";
+import { getAllGuides, getGuideById, GuideType } from "../../lib/guide";
+import siteConfig from "../../content/site.json";
+import Helmet from "../../components/helmet";
 
-export default class Guide extends Component {
-  render() {
-    return (
-      <div>guide</div>
-    )
-  }
+
+type GuideProps = {
+  guide: GuideType;
+};
+
+export default function Guide(props: GuideProps) {
+  const { guide } = props;
+  const GuideContent = require(`../../content/guides/${guide.id}.md`).default;
+
+  return (
+    <Box bg="white" minH="100vh">
+      <GlobalHeader />
+      <Helmet title={guide.title} description={guide.description} />
+      <Box mb="60px">
+        <ContentPageHeader
+          title={guide.title}
+          subtitle={guide.description}
+          formattedDate={`${guide?.type
+            ?.charAt(0)
+            .toUpperCase()}${guide?.type?.slice(1)} Guide`}
+          author={{
+            twitter: guide?.author?.twitter!,
+            picture: guide?.author?.picture!,
+            name: guide?.author?.name!,
+          }}
+          subLink={{
+            text: "Improve this Guide",
+            url: `${siteConfig.url.repo}/tree/master/content/guides/${guide.id}.md`,
+          }}
+        />
+        <Container maxW={"container.md"} position="relative">
+          <MdRenderer>
+            <GuideContent />
+          </MdRenderer>
+        </Container>
+      </Box>
+
+]
+      <Footer />
+    </Box>
+  );
+}
+
+type StaticPathItem = {
+  params: {
+    guide: string;
+  };
+};
+
+export async function getStaticPaths() {
+  const guides = getAllGuides();
+  const paramsList: StaticPathItem[] = guides.map((guide) => ({
+    params: { guide: guide.id },
+  }));
+
+  return {
+    paths: paramsList,
+    fallback: false,
+  };
+}
+
+type ContextType = {
+  params: {
+    guide: string;
+  };
+};
+
+export async function getStaticProps(context: ContextType) {
+  const guideId: string = context?.params?.guide;
+
+  return {
+    props: {
+      guide: getGuideById(guideId),
+    },
+  };
 }
